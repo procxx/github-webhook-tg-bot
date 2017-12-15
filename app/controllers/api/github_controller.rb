e03b87bcff34e47957df2a0e9f6a69cbbc1eff04
crackedmind <digital.stream.of.mind@gmail.com>
2017-12-15 20:46:21 +0300

    Move github controller to api namespace

--- app/controllers/api/github_controller.rb
+++ app/controllers/api/github_controller.rb
@@ -1,4 +1,4 @@
-class GithubController < ActionController::API
+class Api::GithubController < ActionController::API
   def hook
     Rails.logger.info params.inspect
     head :no_content
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match 'github/hook' => 'github#hook', via: [:get, :post]
+  namespace :api do
+    match 'github/hook' => 'github#hook', via: [:get, :post]
+  end
   root to: 'home#index'
 end
