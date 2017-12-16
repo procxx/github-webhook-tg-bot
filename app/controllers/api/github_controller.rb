2c8f32e0e980368b4b1cc9de71d5219e215b75a0
crackedmind <digital.stream.of.mind@gmail.com>
2017-12-16 06:15:43 +0300

    Some more tests

--- app/controllers/api/github_controller.rb
+++ app/controllers/api/github_controller.rb
@@ -1,6 +1,6 @@
 class Api::GithubController < ActionController::API
   def hook
-    Rails.logger.info request.headers['X-GitHub-Event']
-    render json: {status: :ok}, status: 200
+    Rails.logger.info request.headers['HTTP_X_GITHUB_EVENT']
+    render json: {ok: true, url: api_github_hook_url}, status: 200
   end
 end
