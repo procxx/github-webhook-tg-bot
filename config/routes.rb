f4cf2978007aad418c584a36c0dd8e5f9a39c68d
crackedmind <digital.stream.of.mind@gmail.com>
2017-12-16 08:35:32 +0300

    Add route to telegram

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     match 'github/hook' => 'github#hook', via: [:get, :post]
+    match 'telegram/hook' => 'telegram#hook', via: [:get, :post]
   end
   root to: 'home#index'
 end
